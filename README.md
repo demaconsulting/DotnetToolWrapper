192fd69b0e3e08021b0e87334307d1be7c6a4e99
Malcolm Nixon <malcolm.nixon@elbitsystems-us.com>
2024-03-11 12:57:15 -0400

    Initial revision.

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# DotnetToolWrapper
-Package native tools in a Dotnet tool container
+![GitHub forks](https://img.shields.io/github/forks/demaconsulting/DotnetToolWrapper?style=plastic)
+![GitHub Repo stars](https://img.shields.io/github/stars/demaconsulting/DotnetToolWrapper?style=plastic)
+![GitHub contributors](https://img.shields.io/github/contributors/demaconsulting/DotnetToolWrapper?style=plastic)
+![GitHub](https://img.shields.io/github/license/demaconsulting/DotnetToolWrapper?style=plastic)
+
+# About
+
+This project generates a .NET 6.0 console application suitable for use in Dotnet Tool packages to wrap a native application.
+
+# Usage
+
+To create a DotNet tool for an existing application:
+
+1. Create a .nuspec file for the Dotnet tool
+2. Create a `tools/net6.0/any/DotnetToolSettings.xml` file which points to this DotnetToolWrapper
+3. Create a `tools/net6.0/any/DotnetToolWrapper.json` file describing the existing application to run
+4. Copy this DotnetToolWrapper application (.dll) into the `tools/net6.0/any` folder
+5. Add the existing application files under the `tools/net6.0/any` folder.
+6. Package the NuGet package
+
+## Folder Structure
+
+The following is an example folder structure for a tool:
+
+| Path | Description |
+| :-------- | :----- |
+| /tool.nuspec | Tool Nuspec file |
+| /README.md | Tool README file |
+| /tools/net6.0/any/DotnetToolSettings.xml | Dotnet tool settings |
+| /tools/net6.0/any/DotnetToolWrapper.json | DotnetToolWrapper application settings |
+| /tools/net6.0/any/DemaConsulting.DotnetToolWrapper.deps.json | DotnetToolWrapper dependencies |
+| /tools/net6.0/any/DemaConsulting.DotnetToolWrapper.dll | DotnetToolWrapper application |
+| /tools/net6.0/any/DemaConsulting.DotnetToolWrapper.runtimeconfig.json | DotnetToolWrapper runtime |
+
+## Nuspec File
+
+The following is a sample .nuspec file for a tool:
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+    <metadata>
+        <id>My.Tool.Package</id>
+        <version>0.0.0</version>
+        <title>Title of Tool</title>
+        <authors>Author Name</authors>
+        <license type="expression">MIT</license>
+        <readme>docs/README.md</readme>
+        <description>Description of Tool</description>
+        <packageTypes>
+            <packageType name="DotnetTool" />
+        </packageTypes>
+    </metadata>
+    <files>
+        <file src="README.md" target="docs/README.md" />
+        <file src="tools/**/*" target="." />
+    </files>
+</package>
+```
+
+It copies the README.md and the tools folder into the nuget package.
+
+## DotnetToolSettings.xml
+
+The following is a sample DotnetToolSettings.xml file:
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<DotNetCliTool Version="1">
+  <Commands>
+    <Command Name="my-tool" EntryPoint="DemaConsulting.DotnetToolWrapper.dll" Runner="dotnet" />
+  </Commands>
+</DotNetCliTool>
+```
+
+The `Name` should be customized to match the desired name of the Dotnet tool. Dotnet uses this information when installing the package.
+
+## DotnetToolWrapper.json
+
+The following is a sample DotnetToolWrapper.json file:
+
+```json
+{
+  "win-x64": {
+    "program": "win-x64/my-program.exe"
+  },
+  "linux-x64": {
+    "program": "linux-x64/my-program"
+  }
+}
+```
+
+This defines the program to run for each operating system.
+
+## Packaging
+
+To create the Dotnet tool Nuget package run `dotnet pack -Version <x.y.z>` specifying the version of the package to create.
---
+++ src/DemaConsulting.DotnetToolWrapper/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace DemaConsulting.DotnetToolWrapper;
+
+/// <summary>
+/// Dotnet Tool Wrapper Program
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Get the operating system
+    /// </summary>
+    /// <returns>Operating system name</returns>
+    public static string GetOs()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return "win";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return "linux";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD)) return "freebsd";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) return "osx";
+        return "unknown";
+    }
+
+    /// <summary>
+    /// Get the architecture
+    /// </summary>
+    /// <returns>Architecture name</returns>
+    public static string GetArchitecture()
+    {
+        return RuntimeInformation.OSArchitecture switch
+        {
+            Architecture.X86 => "x86",
+            Architecture.X64 => "x64",
+            Architecture.Arm => "arm",
+            Architecture.Arm64 => "arm64",
+            Architecture.Wasm => "wasm",
+            Architecture.S390x => "s390x",
+            _ => "unknown"
+        };
+    }
+
+    /// <summary>
+    /// Get the target string
+    /// </summary>
+    /// <returns>Target string</returns>
+    public static string GetTarget()
+    {
+        return $"{GetOs()}-{GetArchitecture()}";
+    }
+
+    /// <summary>
+    /// Application entry point
+    /// </summary>
+    /// <param name="args">Program arguments</param>
+    public static void Main(string[] args)
+    {
+        // Get the target
+        var target = GetTarget();
+
+        // Get the working directory
+        var workingDirectory = Directory.GetCurrentDirectory();
+
+        // Get the tool location
+        var location = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+        if (string.IsNullOrEmpty(location))
+            location = workingDirectory;
+
+        // Get the configuration file path
+        var configPath = Path.Combine(location, "DotnetToolWrapper.json");
+        if (!File.Exists(configPath))
+        {
+            Console.WriteLine($"Missing configuration file {configPath}");
+            Environment.Exit(1);
+        }
+
+        // Read the configuration
+        var config = JsonDocument.Parse(File.ReadAllText(configPath));
+
+        // Get the target configuration
+        if (!config.RootElement.TryGetProperty(target, out var targetConfig))
+        {
+            Console.WriteLine($"This tool does not support the {target} target");
+            Environment.Exit(1);
+        }
+
+        // Get the application
+        if (!targetConfig.TryGetProperty("program", out var program))
+        {
+            Console.WriteLine($"Bad configuration for {target} target");
+            Environment.Exit(1);
+        }
+
+        // Get the program name
+        var programName = program.ToString();
+        programName = Environment.ExpandEnvironmentVariables(programName);
+        programName = Path.GetFullPath(programName, location);
+
+        // Construct the process start information
+        var startInfo = new ProcessStartInfo(programName)
+        {
+            WorkingDirectory = workingDirectory,
+            UseShellExecute = false
+        };
+        foreach (var arg in args)
+            startInfo.ArgumentList.Add(arg);
+
+        // Run the process
+        var process = Process.Start(startInfo);
+        if (process == null)
+        {
+            Console.WriteLine($"Unable to start process {programName}");
+            Environment.Exit(1);
+        }
+
+        // Wait for the process to exit
+        process.WaitForExit();
+
+        // Exit with the process exit code
+        Environment.Exit(process.ExitCode);
+    }
+}
